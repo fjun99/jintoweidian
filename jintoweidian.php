9c8f1b2f52ae935830fa5975901d3b710e3b32d6
fangjun <fjun99@gmail.com>
2015-03-25 19:13:57 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -3,9 +3,26 @@
 /*
 Plugin Name: Jintoweidian
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Description: receive data from jinshuju and update it to weidian
 Version: 1.0
 Author: fangjun
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
+
+define('JIN_PLUGIN_DIR', WP_PLUGIN_DIR.'/'. dirname(plugin_basename(__FILE__)));
+
+add_action('init', 'jintoweidian_init',11);
+function jintoweidian_init($wp){
+
+    $file  = JIN_PLUGIN_DIR.'/log.txt';//要写入文件的文件名（可以是任意文件名），如果文件不存在，将会创建一个
+
+    if(isset($_GET['jin']) ){
+        $content = "第一次写入的内容\n";
+        if($f  = file_put_contents($file, $content,FILE_APPEND)){
+
+        }
+
+    }
+
+}
