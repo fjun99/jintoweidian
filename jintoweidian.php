7338b15a31221ecde73db8db31e8786a48dbc49f
fangjun <fjun99@gmail.com>
2015-03-26 21:57:53 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -13,7 +13,7 @@ License: A "Slug" license name e.g. GPL2
 define('JIN_PLUGIN_DIR', WP_PLUGIN_DIR.'/'. dirname(plugin_basename(__FILE__)));
 define('weidian_key', '620889');
 define('weidian_secret', '106e6c955826149d13ae025e8e44424b');
-define('url_get_weidian_topen','https://api.vdian.com/token?grant_type=client_credential&appkey='.weidian_key.'&secret='.weidian_secret);
+define('url_get_weidian_token','https://api.vdian.com/token?grant_type=client_credential&appkey='.weidian_key.'&secret='.weidian_secret);
 //https://api.vdian.com/token?grant_type=client_credential&appkey=620889&secret=106e6c955826149d13ae025e8e44424b
 
 add_action('init', 'jintoweidian_init',11);
@@ -37,11 +37,11 @@ function jintoweidian_init($wp){
             header('HTTP/1.1 200 OK');
         }
 
-        echo url_get_weidian_topen;
-        $response = http_get(url_get_weidian_topen, array("timeout"=>1), $info);
+        echo url_get_weidian_token;
+        $response = http_get(url_get_weidian_token, array("timeout"=>1), $res);
 //        echo url_get_weidian_topen;
 //        echo $info;
-        file_put_contents($file, $info,FILE_APPEND);
+        file_put_contents($file, $res,FILE_APPEND);
 
     }
 
