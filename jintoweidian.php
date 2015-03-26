e63c88ef9724d1bced6730c7b292cf5ddcd8da67
fangjun <fjun99@gmail.com>
2015-03-26 21:45:56 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -13,8 +13,7 @@ License: A "Slug" license name e.g. GPL2
 define('JIN_PLUGIN_DIR', WP_PLUGIN_DIR.'/'. dirname(plugin_basename(__FILE__)));
 define('weidian_key', '620889');
 define('weidian_secret', '106e6c955826149d13ae025e8e44424b');
-define('url_get_weidian_topen','https://api.vdian.com/token?grant_type=client_credential&appkey='.
-    weidian_key.'&secret='.weidian_secret);
+define('url_get_weidian_topen','https://api.vdian.com/token?grant_type=client_credential&appkey='.weidian_key.'&secret='.weidian_secret);
 
 add_action('init', 'jintoweidian_init',11);
 function jintoweidian_init($wp){
@@ -34,7 +33,7 @@ function jintoweidian_init($wp){
 
 //        $content ='test\n';
         if($f  = file_put_contents($file, $content,FILE_APPEND)){
-//            header('HTTP/1.1 200 OK');
+            header('HTTP/1.1 200 OK');
         }
 
         $response = http_get(url_get_weidian_topen, array("timeout"=>1), $info);
