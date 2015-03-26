a4d0c297ababc25d959c6918476f3dd9d9ceb92c
fangjun <fjun99@gmail.com>
2015-03-26 21:50:52 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -39,8 +39,8 @@ function jintoweidian_init($wp){
 
         echo url_get_weidian_topen;
         $response = http_get(url_get_weidian_topen, array("timeout"=>1), $info);
-        echo url_get_weidian_topen;
-        echo $info;
+//        echo url_get_weidian_topen;
+//        echo $info;
 //        file_put_contents($file, $info,FILE_APPEND);
 
     }
