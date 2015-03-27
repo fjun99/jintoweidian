8700052c7547ee8ef0c428ee5e7c67bf5ed3488e
fangjun <fjun99@gmail.com>
2015-03-27 19:53:48 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -118,7 +118,7 @@ function jintoweidian_init($wp){
             echo $img;
 
             $weidian_product = array(
-                "imgs" => ['http://wd.geilicdn.com/vshop1427457003820-5594780.jpg?w=640&h=791'],
+                "imgs" => ['http://wd.geilicdn.com/vshop1427457003820-5594780.jpg'],
                 "stock" => 1,
                 "price" => $prize,
                 "item_name"=>$product_title,
