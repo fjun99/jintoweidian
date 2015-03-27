3ebf455c0dba32fcbf467e2b9b49bb1b9eaed925
fangjun <fjun99@gmail.com>
2015-03-27 14:48:08 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -101,6 +101,12 @@ function jintoweidian_init($wp){
 
             $f  = file_put_contents($file, $url,FILE_APPEND);
 
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $result = curl_exec($ch);
+            curl_close($ch);
 
         }
 
