0e649d756d1e1d73107b602c7bd8d46a6bcef763
fangjun <fjun99@gmail.com>
2015-03-27 15:37:37 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -112,7 +112,7 @@ function jintoweidian_init($wp){
             $product_title = $product_title.'主人：'.$owner."\n";
 
             $imgs = array($img);
-            var_dump(imgs);
+            var_dump($imgs);
             echo "\n";
 
             $weidian_product = array(
