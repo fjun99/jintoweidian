0158de467b5b2fad9cbc6d10b9badfe680c3aa12
fangjun <fjun99@gmail.com>
2015-03-27 20:11:56 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -48,7 +48,7 @@ function jintoweidian_init($wp){
             $url = url_weidian_add_product.$result['access_token'].url_weidian_add_product_part2;
 
 
-            $jin_data = '{"form":"9rk3Dk","entry":{"serial_number":2,"field_1":"https://dn-jsjpri.qbox.me/en/5514f1bd41505068a58f0200/2_1_mamifair_%E5%9B%BE.png?token=kTs1p9Tn1gGWiIC_O83TcJeBc2E7oVxVCgDuTGFj:XjeM9m3yDreyfOr281Isemjje-Q=:eyJTIjoiZG4tanNqcHJpLnFib3gubWUvZW4vNTUxNGYxYmQ0MTUwNTA2OGE1OGYwMjAwLzJfMV9tYW1pZmFpcl_lm74ucG5nKiIsIkUiOjE0Mjc0Mzk2MDh9\u0026download","field_2":"测试商品","field_8":"onefangjun","field_3":"全新","field_4":"上海","field_5":100,"field_6":"","field_7":"","creator_name":"mamifair","created_at":"2015-03-27T06:00:18Z","updated_at":"2015-03-27T06:00:18Z","info_remote_ip":"106.120.85.234"}}';
+            $jin_data = '{"form":"9rk3Dk","entry":{"serial_number":3,"field_1":"https://dn-jsjpri.qbox.me/en/551548c84150507c7f750300/3_1_21_3_1175555795.jpg?token=kTs1p9Tn1gGWiIC_O83TcJeBc2E7oVxVCgDuTGFj:9SlHHBkuTnkAiZTU2Ls0zcAn2kE=:eyJTIjoiZG4tanNqcHJpLnFib3gubWUvZW4vNTUxNTQ4Yzg0MTUwNTA3YzdmNzUwMzAwLzNfMV8yMV8zXzExNzU1NTU3OTUuanBnKiIsIkUiOjE0Mjc0NjE4NTN9\u0026download","field_2":"ceshe","field_8":"onefangjun","field_3":"全新","field_4":"北京","field_5":101,"field_6":"L","field_7":"无","creator_name":"mamifair","created_at":"2015-03-27T12:10:53Z","updated_at":"2015-03-27T12:10:53Z","info_remote_ip":"140.206.88.176"}}';
 
             $data = json_decode($jin_data,true);
             $product = $data["entry"];
