f80a857c3096a980467160b0a1bad4c34f58ae6f
fangjun <fjun99@gmail.com>
2015-03-27 14:18:49 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -11,7 +11,7 @@ License: A "Slug" license name e.g. GPL2
 */
 
 define('JIN_PLUGIN_DIR', WP_PLUGIN_DIR.'/'. dirname(plugin_basename(__FILE__)));
-define('weidian_key', '620889');
+define('weidian_key', '6208891');
 define('weidian_secret', '106e6c955826149d13ae025e8e44424b');
 define('url_get_weidian_token','https://api.vdian.com/token?grant_type=client_credential&appkey='.weidian_key.'&secret='.weidian_secret);
 //https://api.vdian.com/token?grant_type=client_credential&appkey=620889&secret=106e6c955826149d13ae025e8e44424b
@@ -43,6 +43,8 @@ function jintoweidian_init($wp){
 
 
         $result   = json_decode($result,true)['result'];
+        $nresult = var_export($result,1);
+        file_put_contents($file, $nresult.'\n\n',FILE_APPEND);
         file_put_contents($file, $result['access_token'].'\n\n',FILE_APPEND);
 
         $data = '{"form":"9rk3Dk","entry":{"serial_number":2,"field_1":"https://dn-jsjpri.qbox.me/en/5514f1bd41505068a58f0200/2_1_mamifair_%E5%9B%BE.png?token=kTs1p9Tn1gGWiIC_O83TcJeBc2E7oVxVCgDuTGFj:XjeM9m3yDreyfOr281Isemjje-Q=:eyJTIjoiZG4tanNqcHJpLnFib3gubWUvZW4vNTUxNGYxYmQ0MTUwNTA2OGE1OGYwMjAwLzJfMV9tYW1pZmFpcl_lm74ucG5nKiIsIkUiOjE0Mjc0Mzk2MDh9\u0026download","field_2":"测试商品","field_8":"onefangjun","field_3":"全新","field_4":"上海","field_5":100,"field_6":"","field_7":"","creator_name":"mamifair","created_at":"2015-03-27T06:00:18Z","updated_at":"2015-03-27T06:00:18Z","info_remote_ip":"106.120.85.234"}}';
