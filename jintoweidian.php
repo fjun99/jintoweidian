c4bd30c22358ded4aed738373cb739e371a74557
fangjun <fjun99@gmail.com>
2015-03-28 14:08:22 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -157,7 +157,7 @@ function api_request($url){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, false);
+//    curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
 
