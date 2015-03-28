ac3bd9a70c6d834048d054de3200d55c4a8d3e2f
fangjun <fjun99@gmail.com>
2015-03-28 17:18:00 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -83,10 +83,9 @@ function jintoweidian_init($wp){
     }
     $product_title = $product_title.'主人：'.$owner."\n";
 
-
+/*
     writelog("====end===");
     exit(0);
-
 
 //下载图片
     $upfilename =  savefile($imgurl);
@@ -109,8 +108,8 @@ function jintoweidian_init($wp){
         exit(0);
     writelog('after img');
     $wimg = substr($wimg,0,-strlen($wimg)+strpos($wimg,'?'));
-
-//    $wimg = 'http://wd.geilicdn.com/vshop395640-1390204649-1.jpg';
+*/
+    $wimg = 'http://wd.geilicdn.com/vshop395640-1390204649-1.jpg';
 
     $weidian_product = array(
         "imgs" => [$wimg],
