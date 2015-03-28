6a701bf783fbe7ead013ada60dc1dcabcef82233
fangjun <fjun99@gmail.com>
2015-03-28 13:12:24 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -121,7 +121,7 @@ function api_request_add($url){
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 //    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
 
