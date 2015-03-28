ca87a632220b96765baf87b960bd8bba11db5563
fangjun <fjun99@gmail.com>
2015-03-28 19:57:14 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -188,6 +188,7 @@ function savefile($url){
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $raw=curl_exec($ch);
 
