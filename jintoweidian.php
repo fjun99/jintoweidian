6b75f150ae4e01e50e36f03d94bd017ba55b879b
fangjun <fjun99@gmail.com>
2015-03-30 14:29:10 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -18,6 +18,7 @@ define('url_get_weidian_token','https://api.vdian.com/token?grant_type=client_cr
 define('url_weidian_add_product','http://api.vdian.com/api?public={"method":"vdian.item.add","access_token":"');
 define('url_weidian_add_product_part2','","version":"1.0","format":"json"}&param=');
 define('url_weidian_upload','http://api.vdian.com/media/upload?access_token=');
+define('option_name','jindata');
 
 //https://api.vdian.com/token?grant_type=client_credential&appkey=620889&secret=106e6c955826149d13ae025e8e44424b
 
@@ -40,101 +41,18 @@ function jintoweidian_init($wp){
         flush();
 
         writelog($jin_data);
+        if(get_option(option_name)) {
+            add_option(option_name, $jin_data, null, no);
+        }else {
+            update_option(option_name,$jin_data);
+        }
 
+        wp_schedule_single_event( time() + 120, 'pushweidian' );
     }else{
         //
     }
 
 
-
-    //get weidian token
-    $result = api_request(url_get_weidian_token);
-
-
-
-    $json = json_decode($result,true);
-    $token_result = isset($json['result']) ? $json['result'] : null;
-
-    writelog('before token');
-    if($token_result == null)
-        exit(0);
-    writelog('after token');
-
-    $token = $token_result['access_token'];
-
-
-    $data = json_decode($jin_data,true);
-    $product = $data["entry"];
-
-    $img =  $product['field_1'];
-    $imgurl= substr($img,0,-9);
-    $title = $product['field_2'];
-    $owner = $product['field_8'];
-    $new =  $product['field_3'];
-    $location = $product['field_4'];
-    $prize = $product['field_5'];
-    $size = $product['field_6'];
-    $desc = $product['field_7'];
-
-
-    $product_title = '【'.$owner.'】'.$title."\n";
-    if($desc!=''){
-        $product_title = $product_title.$desc."\n";
-    }
-    $product_title = $product_title.'成色：'.$new."\n";
-    $product_title = $product_title.'所在地：'.$location."\n";
-    if($size!=''){
-        $product_title = $product_title.'尺码：'.$size."\n";
-    }
-    $product_title = $product_title.'主人：'.$owner."\n";
-
-
-/*
-//下载图片
-    $upfilename =  savefile($imgurl);
-
-
-//    $upfilename = '103_1_images.jpeg';
-//上传图片
-
-    $upload_url = url_weidian_upload.$token;
-    $file_name = JIN_PLUGIN_DIR.'/temp/'.$upfilename;
-    $upresult = api_upload($upload_url,$file_name);
-
-
-
-    $image_result = json_decode($upresult,true);
-    $wimg = isset($image_result['result']) ? $image_result['result'] : null;
-
-    writelog('before img');
-    if($wimg==null)
-        exit(0);
-    writelog('after img');
-    $wimg = substr($wimg,0,-strlen($wimg)+strpos($wimg,'?'));
-*/
-    $wimg = 'http://wd.geilicdn.com/vshop395640-1390204649-1.jpg';
-
-    $weidian_product = array(
-        "imgs" => [$wimg],
-        "stock" => 1,
-        "price" => $prize,
-        "item_name"=>$product_title,
-        "fx_fee_rate"=>"1",
-        "cate_ids"=>[36660506],
-        "skus"=>[],
-        "merchant_code"=>"",
-    );
-
-    $weidian_product_json = json_encode($weidian_product,true);
-
-    $add_product_url = url_weidian_add_product.$token.url_weidian_add_product_part2;
-    $add_product_url = $add_product_url.$weidian_product_json;
-
-
-
-
-    $result= api_request($add_product_url);
-    writelog($result);
 
 
     writelog("====end===");
@@ -222,4 +140,105 @@ function writelog($text){
     $file  = JIN_PLUGIN_DIR.'/log.txt';
     $text = $text."\n\n";
     $f  = file_put_contents($file, $text,FILE_APPEND);
+}
+
+
+add_action( 'pushweidian', 'push_to_weidian', 10, 3 );
+function push_to_weidian() {
+
+    $jin_data = get_option(option_name);
+
+
+    //get weidian token
+    $result = api_request(url_get_weidian_token);
+
+
+
+    $json = json_decode($result,true);
+    $token_result = isset($json['result']) ? $json['result'] : null;
+
+    writelog('before token');
+    if($token_result == null)
+        exit(0);
+    writelog('after token');
+
+    $token = $token_result['access_token'];
+
+
+    $data = json_decode($jin_data,true);
+    $product = $data["entry"];
+
+    $img =  $product['field_1'];
+    $imgurl= substr($img,0,-9);
+    $title = $product['field_2'];
+    $owner = $product['field_8'];
+    $new =  $product['field_3'];
+    $location = $product['field_4'];
+    $prize = $product['field_5'];
+    $size = $product['field_6'];
+    $desc = $product['field_7'];
+
+
+    $product_title = '【'.$owner.'】'.$title."\n";
+    if($desc!=''){
+        $product_title = $product_title.$desc."\n";
+    }
+    $product_title = $product_title.'成色：'.$new."\n";
+    $product_title = $product_title.'所在地：'.$location."\n";
+    if($size!=''){
+        $product_title = $product_title.'尺码：'.$size."\n";
+    }
+    $product_title = $product_title.'主人：'.$owner."\n";
+
+
+
+//下载图片
+    $upfilename =  savefile($imgurl);
+
+
+//    $upfilename = '103_1_images.jpeg';
+//上传图片
+
+    $upload_url = url_weidian_upload.$token;
+    $file_name = JIN_PLUGIN_DIR.'/temp/'.$upfilename;
+    $upresult = api_upload($upload_url,$file_name);
+
+
+
+    $image_result = json_decode($upresult,true);
+    $wimg = isset($image_result['result']) ? $image_result['result'] : null;
+
+    writelog('before img');
+    if($wimg==null)
+        exit(0);
+    writelog('after img');
+    $wimg = substr($wimg,0,-strlen($wimg)+strpos($wimg,'?'));
+
+//    $wimg = 'http://wd.geilicdn.com/vshop395640-1390204649-1.jpg';
+
+    $weidian_product = array(
+        "imgs" => [$wimg],
+        "stock" => 1,
+        "price" => $prize,
+        "item_name"=>$product_title,
+        "fx_fee_rate"=>"1",
+        "cate_ids"=>[36660506],
+        "skus"=>[],
+        "merchant_code"=>"",
+    );
+
+    $weidian_product_json = json_encode($weidian_product,true);
+
+    $add_product_url = url_weidian_add_product.$token.url_weidian_add_product_part2;
+    $add_product_url = $add_product_url.$weidian_product_json;
+
+
+
+
+    $result= api_request($add_product_url);
+    writelog($result);
+
+
+
+    update_option(option_name,'');
 }
