b12323a692391f4a9a54e3e5941eedc2b43a33c5
fangjun <fjun99@gmail.com>
2015-04-03 22:02:21 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -316,17 +316,17 @@ function resizeImageIfNeed($filedir,$filename){
 
     writelog("======resize image====");
 
-    $width=1000; //*** Fix Width & Heigh (Autu caculate) ***//
-    $size=GetimageSize($images);
-    $height=round($width*$size[1]/$size[0]);
-    $images_orig = ImageCreateFromJPEG($images);
-    $photoX = ImagesX($images_orig);
-    $photoY = ImagesY($images_orig);
-    $images_fin = ImageCreateTrueColor($width, $height);
-    ImageCopyResampled($images_fin, $images_orig, 0, 0, 0, 0, $width+1, $height+1, $photoX, $photoY);
-    ImageJPEG($images_fin,$new_images);
-    ImageDestroy($images_orig);
-    ImageDestroy($images_fin);
+//    $width=1000; //*** Fix Width & Heigh (Autu caculate) ***//
+//    $size=GetimageSize($images);
+//    $height=round($width*$size[1]/$size[0]);
+//    $images_orig = ImageCreateFromJPEG($images);
+//    $photoX = ImagesX($images_orig);
+//    $photoY = ImagesY($images_orig);
+//    $images_fin = ImageCreateTrueColor($width, $height);
+//    ImageCopyResampled($images_fin, $images_orig, 0, 0, 0, 0, $width+1, $height+1, $photoX, $photoY);
+//    ImageJPEG($images_fin,$new_images);
+//    ImageDestroy($images_orig);
+//    ImageDestroy($images_fin);
 
     return $new_images;
 }
