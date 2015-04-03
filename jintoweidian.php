542325fecf42fd72a549ed650005e5574c65f76b
fangjun <fjun99@gmail.com>
2015-04-03 23:08:45 +0800

    first

--- jintoweidian.php
+++ jintoweidian.php
@@ -316,17 +316,17 @@ function resizeImageIfNeed($filedir,$filename){
     $width=1000; //*** Fix Width & Heigh (Autu caculate) ***//
     $size=GetimageSize($images);
     $height=round($width*$size[1]/$size[0]);
-//    $images_orig = ImageCreateFromJPEG($images);
-//    $photoX = ImagesX($images_orig);
-//    $photoY = ImagesY($images_orig);
-//    $images_fin = ImageCreateTrueColor($width, $height);
-//    ImageCopyResampled($images_fin, $images_orig, 0, 0, 0, 0, $width+1, $height+1, $photoX, $photoY);
-//    ImageJPEG($images_fin,$new_images);
-//    ImageDestroy($images_orig);
-//    ImageDestroy($images_fin);
+    $images_orig = ImageCreateFromJPEG($images);
+    $photoX = ImagesX($images_orig);
+    $photoY = ImagesY($images_orig);
+    $images_fin = ImageCreateTrueColor($width, $height);
+    ImageCopyResampled($images_fin, $images_orig, 0, 0, 0, 0, $width+1, $height+1, $photoX, $photoY);
+    ImageJPEG($images_fin,$new_images);
+    ImageDestroy($images_orig);
+    ImageDestroy($images_fin);
 
-//    return $new_images;
-
+    return $new_images;
+/*
     if( extension_loaded('imagick') || class_exists("Imagick") ){
         writelog("im good");
     }else{
@@ -357,4 +357,5 @@ function resizeImageIfNeed($filedir,$filename){
     writelog('end image resize');
 
     return $new_images;
+*/
 }
